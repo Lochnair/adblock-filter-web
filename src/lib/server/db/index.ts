00fd9f0cc089330b722965168fa77787f5c2b957
Nils Andreas Svee <me@lochnair.net>
2025-07-02 23:55:50 +0800

    chore: run prettier formatting

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from 'drizzle-orm/d1';
 
 export function getDB(platform: App.Platform | Readonly<App.Platform> | undefined) {
-       if (!platform?.env?.DB) {
-              throw new Error('DB binding is not available');
-       }
+	if (!platform?.env?.DB) {
+		throw new Error('DB binding is not available');
+	}
 
-       return drizzle(platform.env.DB);
+	return drizzle(platform.env.DB);
 }
