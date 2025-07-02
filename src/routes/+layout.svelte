0438e8054bcf96eafc1b1b4abdc14bfa5e94c160
Nils Andreas Svee <me@lochnair.net>
2025-07-02 21:03:19 +0800

    chore: change nav title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <Navbar class="mb-4 border-b">
-	<NavBrand href="/" class="text-xl font-semibold">DNS Tool</NavBrand>
+	<NavBrand href="/" class="text-xl font-semibold">Adblock DNS filters</NavBrand>
 	<NavHamburger class="ml-auto md:hidden" />
 	<NavUl class="ml-auto flex flex-col md:flex-row">
 		<NavLi href="/filter.txt" class="px-2 py-1">Filter</NavLi>
