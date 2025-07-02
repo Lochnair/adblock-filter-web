75d01b5bbcf3f43b590c60db4ff8c0e0d20821d0
Nils Andreas Svee <me@lochnair.net>
2025-07-03 01:11:09 +0800

    chore: formats NavBar component
    Formats the NavBar component for improved readability
    and consistency.

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <Navbar>
-  <NavBrand href="/">
-    <!--<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
-    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Adblock DNS filters</span>
-  </NavBrand>
+	<NavBrand href="/">
+		<!--<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+			>Adblock DNS filters</span
+		>
+	</NavBrand>
 
-  <DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-800" />
+	<DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-800" />
 </Navbar>
