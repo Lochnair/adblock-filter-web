4a3dc1e5f5f1a15039eeb3c6835a48fbfe56d1a3
Nils Andreas Svee <me@lochnair.net>
2025-07-03 00:25:10 +0800

    chore: navbar changes

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, DarkMode } from 'flowbite-svelte';
 </script>
 
-<Navbar class="mb-4 border-b">
-	<NavBrand href="/" class="text-xl font-semibold">Adblock DNS filters</NavBrand>
-	<NavHamburger class="ml-auto md:hidden" />
-	<NavUl class="ml-auto flex flex-col md:flex-row">
-		<NavLi href="/filter.txt" class="px-2 py-1">Filter</NavLi>
-	</NavUl>
+<Navbar>
+  <NavBrand href="/">
+    <!--<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Adblock DNS filters</span>
+  </NavBrand>
+
+  <DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-800" />
 </Navbar>
