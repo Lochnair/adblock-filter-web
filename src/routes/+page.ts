ff33c2865d918f56ed0e9cea859b23baefa34846
Nils Andreas Svee <me@lochnair.net>
2026-04-04 16:20:34 +0200

    feat(app): Integrate new features into main page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,11 +2,15 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, url }) => {
 	const slug = url.searchParams.get('list') ?? 'default';
-	const [listsRes, recordsRes] = await Promise.all([
+	const [listsRes, recordsRes, sitesRes, keysRes] = await Promise.all([
 		fetch('/api/lists'),
-		fetch(`/api/records?list=${slug}`)
+		fetch(`/api/records?list=${slug}`),
+		fetch('/api/sites'),
+		fetch('/api/keys')
 	]);
 	const lists = await listsRes.json();
 	const records = await recordsRes.json();
-	return { lists, records, selectedList: slug };
+	const sites = await sitesRes.json();
+	const keys = await keysRes.json();
+	return { lists, records, selectedList: slug, sites, keys };
 };
